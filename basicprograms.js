2db68adf7f4f82d1da62edd50cca623bc6ef4c02
priyankaedulakanti <117647719+priyankaedulakanti@users.noreply.github.com>
2022-11-17 15:31:49 +0530

    Add files via upload

---
+++ basicprograms.js
@@ -0,0 +1,26 @@
+//Odd numbers
+// for (let i=1;i<=100;i++)
+// if((i%2)!==0)
+// {
+// console.log(i)
+// }
+
+//Even numbers
+
+// for (let i=1;i<=100;i++)
+// if((i%2)==0)
+// {
+// console.log(i)
+// }
+
+//Multiples of 3
+ for (let i=1;i<=100;i++)
+if((i%3)==0)
+{
+console.log(i)
+}
+
+//let a="welcome123 adadffghh rtyyuuuissdg";
+//console.log(a.toUpperCase());
+
+//console.log(a.substring(3,7))
